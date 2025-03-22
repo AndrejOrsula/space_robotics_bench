ed9e865aeb0155d89b63e5e8bc9e8ab99de2fe31
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-23 00:07:05 +0100

    Update command mapping for task space actions
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/core/action/group/manipulation/task_space.py
+++ srb/core/action/group/manipulation/task_space.py
@@ -15,15 +15,13 @@ class InverseKinematicsActionGroup(ActionGroup):
     delta_twist: DifferentialInverseKinematicsActionCfg = MISSING  # type: ignore
 
     def map_cmd_to_action(self, twist: torch.Tensor, event: bool) -> torch.Tensor:
-        return (
-            twist if self.delta_twist.controller.command_type == "pose" else twist[:3]
-        )
+        if self.delta_twist.controller.command_type == "pose":
+            assert self.delta_twist.controller.use_relative_mode
+            return twist
+        else:
+            return twist[:3]
 
 
 @configclass
 class OperationalSpaceControlActionGroup(ActionGroup):
     delta_twist: OperationalSpaceControllerActionCfg = MISSING  # type: ignore
-
-    def map_cmd_to_action(self, twist: torch.Tensor, event: bool) -> torch.Tensor:
-        # TODO[mid]: Map teleop actions based on the impedance mode of the OSC
-        raise NotImplementedError

3cc2cc22db896fec0f0259d3c42c91374c56f7d3
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-27 18:46:00 +0100

    Update thrust action
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/core/action/group/mobile/__init__.py
+++ srb/core/action/group/mobile/__init__.py
@@ -1,3 +1,4 @@
 from .multicopter import MulticopterBodyAccelerationActionGroup  # noqa: F401
 from .non_holonomic import NonHolonomicActionGroup  # noqa: F401
+from .thrust import ThrustActionGroup  # noqa: F401
 from .wheeled import WheeledRoverActionGroup  # noqa: F401

edfac6c91f6c9ececf6c56814ac026a5e9de066f
Andrej Orsula <orsula.andrej@gmail.com>
2025-08-17 16:43:33 +0200

    WIP: MoveIt2 hardware interfaces
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ srb/interfaces/sim_to_real/hardware/_dummy.py
@@ -0,0 +1,102 @@
+from functools import cached_property
+from typing import Dict
+
+import gymnasium
+import numpy
+
+from srb.interfaces.sim_to_real.core.hardware import (
+    HardwareInterface,
+    HardwareInterfaceCfg,
+)
+from srb.utils import logging
+
+
+class DummyInterfaceCfg(HardwareInterfaceCfg):
+    pass
+
+
+class DummyInterface(HardwareInterface):
+    cfg: DummyInterfaceCfg
+
+    def __init__(self, cfg: DummyInterfaceCfg = DummyInterfaceCfg()):
+        super().__init__(cfg)
+
+        self.step_counter: int = 0
+        self.should_reset: bool = False
+
+    def start(self, **kwargs):
+        super().start(**kwargs)
+
+    def close(self):
+        super().close()
+
+    def sync(self):
+        super().sync()
+
+        self.step_counter += 1
+        if self.should_reset:
+            self.should_reset = False
+            self.step_counter = 0
+
+        self.obs = {
+            "state/tf_pos2d_robot_to_target": numpy.array(
+                (1.0, 9.0),
+                dtype=numpy.float32,
+            ),
+            "state/tf_rot2dtrigyaw_robot_to_target": numpy.array(
+                (0.2, 0.8),
+                dtype=numpy.float32,
+            ),
+        }
+        self.rew = numpy.random.random()
+        self.term = numpy.random.random() < (0.001 * self.step_counter)
+
+    def reset(self):
+        super().reset()
+        self.should_reset = True
+
+    @property
+    def supported_action_spaces(self) -> gymnasium.spaces.Dict:
+        return gymnasium.spaces.Dict(
+            {
+                "robot/wheeled_drive": gymnasium.spaces.Box(
+                    low=-1.0, high=1.0, shape=(2,), dtype=numpy.float32
+                )
+            }
+        )
+
+    @cached_property
+    def action_scale_linear(self) -> float:
+        return self._action_scale.get(
+            "robot/wheeled_drive_linear"
+        ) or self._action_scale.get("robot/wheeled_drive", 1.0)
+
+    @cached_property
+    def action_scale_angular(self) -> float:
+        return self._action_scale.get(
+            "robot/wheeled_drive_angular"
+        ) or self._action_scale.get("robot/wheeled_drive", 1.0)
+
+    def apply_action(self, action: Dict[str, numpy.ndarray]):
+        assert "robot/wheeled_drive" in action.keys() and action[
+            "robot/wheeled_drive"
+        ].shape == (2,)
+
+        act_linear = self.action_scale_linear * action["robot/wheeled_drive"][0]
+        act_angular = self.action_scale_angular * action["robot/wheeled_drive"][1]
+
+        logging.debug(
+            f'[{self.name}] Applying action: {{"act_linear": {act_linear}, "act_angular": {act_angular}}}'
+        )
+
+    @property
+    def observation(self) -> Dict[str, numpy.ndarray]:
+        return self.obs
+
+    @property
+    def reward(self) -> float:
+        return self.rew
+
+    @property
+    def termination(self) -> bool:
+        return self.term

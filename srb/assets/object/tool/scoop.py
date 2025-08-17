fddfa4e70d6f2080cd807ec4157f64e78f458b36
Andrej Orsula <orsula.andrej@gmail.com>
2025-08-17 21:33:46 +0200

    Add custom scoops
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/assets/object/tool/scoop.py
+++ srb/assets/object/tool/scoop.py
@@ -8,13 +8,17 @@ from srb.core.sim import (
 )
 from srb.utils.path import SRB_ASSETS_DIR_SRB_OBJECT
 
+SRB_ASSETS_DIR_SRB_OBJECT_SCOOP = SRB_ASSETS_DIR_SRB_OBJECT.joinpath("scoop")
+
 
 class ScoopRectangular(Tool):
     asset_cfg: RigidObjectCfg = RigidObjectCfg(
         prim_path="{ENV_REGEX_NS}/scoop",
         spawn=UsdFileCfg(
             usd_path=(
-                SRB_ASSETS_DIR_SRB_OBJECT.joinpath("scoop_rectangular.usdz").as_posix()
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_rectangular.usdz"
+                ).as_posix()
             ),
             activate_contact_sensors=True,
             collision_props=CollisionPropertiesCfg(),
@@ -34,7 +38,9 @@ class ScoopSpherical(Tool):
         prim_path="{ENV_REGEX_NS}/scoop",
         spawn=UsdFileCfg(
             usd_path=(
-                SRB_ASSETS_DIR_SRB_OBJECT.joinpath("scoop_spherical.usdz").as_posix()
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_spherical.usdz"
+                ).as_posix()
             ),
             activate_contact_sensors=True,
             collision_props=CollisionPropertiesCfg(),
@@ -54,7 +60,75 @@ class ScoopTriangular(Tool):
         prim_path="{ENV_REGEX_NS}/scoop",
         spawn=UsdFileCfg(
             usd_path=(
-                SRB_ASSETS_DIR_SRB_OBJECT.joinpath("scoop_triangular.usdz").as_posix()
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_triangular.usdz"
+                ).as_posix()
+            ),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(),
+            mesh_collision_props=MeshCollisionPropertiesCfg(mesh_approximation="sdf"),
+            rigid_props=RigidBodyPropertiesCfg(),
+            mass_props=MassPropertiesCfg(mass=0.001),
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="scoop")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.1)))
+
+
+class ScoopCustom1(Tool):
+    asset_cfg: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/scoop",
+        spawn=UsdFileCfg(
+            usd_path=(
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_custom1.usdz"
+                ).as_posix()
+            ),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(),
+            mesh_collision_props=MeshCollisionPropertiesCfg(mesh_approximation="sdf"),
+            rigid_props=RigidBodyPropertiesCfg(),
+            mass_props=MassPropertiesCfg(mass=0.001),
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="scoop")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.1)))
+
+
+class ScoopCustom2(Tool):
+    asset_cfg: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/scoop",
+        spawn=UsdFileCfg(
+            usd_path=(
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_custom2.usdz"
+                ).as_posix()
+            ),
+            activate_contact_sensors=True,
+            collision_props=CollisionPropertiesCfg(),
+            mesh_collision_props=MeshCollisionPropertiesCfg(mesh_approximation="sdf"),
+            rigid_props=RigidBodyPropertiesCfg(),
+            mass_props=MassPropertiesCfg(mass=0.001),
+        ),
+    )
+
+    ## Frames
+    frame_mount: Frame = Frame(prim_relpath="scoop")
+    frame_tool_centre_point: Frame = Frame(offset=Transform(pos=(0.0, 0.0, 0.1)))
+
+
+class ScoopCustom3(Tool):
+    asset_cfg: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/scoop",
+        spawn=UsdFileCfg(
+            usd_path=(
+                SRB_ASSETS_DIR_SRB_OBJECT_SCOOP.joinpath(
+                    "scoop_custom3.usdz"
+                ).as_posix()
             ),
             activate_contact_sensors=True,
             collision_props=CollisionPropertiesCfg(),
